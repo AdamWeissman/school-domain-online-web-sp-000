eb93c1dd9754ce256cd3e5a651dc47e34d9247e9
Adam Weissman <adam@comedytragedyepic.com>
2019-09-22 16:25:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -34,6 +34,10 @@ class School
   def sort
     r1 = roster.sort
     students_alphabetized = r1.map {|grade, students| {grade => students.sort} }
-    students_alphabetized.map {|item, key_in_item| item[key_in_item]}
+    students_alphabetized.each do |item, key_in_item|
+      probably_unnecessary_hash = {}
+      probably_unnecessary_hash[item] << key_in_item
+      end
+    probably_unnecessary_hash
   end
 end
