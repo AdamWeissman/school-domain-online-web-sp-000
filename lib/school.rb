ea86d4417b748b49cbcbe2277b374d7d174475b0
Adam Weissman <adam@comedytragedyepic.com>
2019-09-22 16:18:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -34,6 +34,6 @@ class School
   def sort
     r1 = roster.sort
     students_alphabetized = r1.map {|grade, students| {grade => students.sort} }
-    students_alphabetized.inspect
+    students_alphabetized.reduce
   end
 end
