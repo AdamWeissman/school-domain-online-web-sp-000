2d8a7f6f52c7b1c4be291ec7f52f25f6a5c6c2ad
Adam Weissman <adam@comedytragedyepic.com>
2019-09-22 15:28:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,9 @@ class School
     @roster = {}
   end
 
-  def grade(student_grade)
-    roster[student_grade]
-  end
+  #def grade(student_grade)
+  #  roster[student_grade]
+  #end
 
   def add_student(student_name, z)
     roster[z] || roster[z] = []
